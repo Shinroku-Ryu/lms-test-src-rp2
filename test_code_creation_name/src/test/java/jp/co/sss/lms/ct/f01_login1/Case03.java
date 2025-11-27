c9a88f6aced3796a4c440bbb0da30ab778a4c2e0
RyuShinroku <x-liu@3sss.co.jp>
2025-11-27 13:28:58 +0900

    add:ログイン画面からコー詳細画面に遷移するテストコードを追記 webElementを使って正常系ログイン操作を行い、実行結果が期待値通りかどうかを確認するテストを作成した 期待値は以下の通り： 画面タイトル：コース詳細 | LMS パンくずリスト表示名：コース詳細

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト ログイン機能①
@@ -36,6 +38,18 @@ public class Case03 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		//トップページへ遷移
+		String topPage = "http://localhost:8080/lms/";
+		goTo(topPage);
+		//待ち処理
+		visibilityTimeout(By.tagName("body"),60);
+		//エビデンスを取得
+		getEvidence(new Case03() {},"トップページへ画面遷移");
+		
+		//検証
+		String expectedTitle = getTitle();
+		String actualTitle = "ログイン | LMS";
+		assertEquals(expectedTitle,actualTitle,"トップページのタイトルが一致しません");		
 	}
 
 	@Test
@@ -43,6 +57,25 @@ public class Case03 {
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
 		// TODO ここに追加
+		//受講生ユーザーIDを入力
+		sendKeysIntoNameElement("loginId","StudentAA01");
+		getEvidence(new Case03() {},"受講生のログインID入力");
+		//パスワードを入力
+		sendKeysIntoNameElement("password","StudentAA011");
+		getEvidence(new Case03() {},"受講生のログインPW入力");
+		//ログインボタンを押下
+		clickButton(".btn.btn-primary");
+		//エビデンスを取得
+		getEvidence(new Case03() {},"受講生ユーザーでログイン");
+		
+		//検証
+		String expectedTitle = "コース詳細 | LMS";
+		String actualTitle = getTitle();
+		assertEquals(expectedTitle,actualTitle,"画面タイトルが一致しません");
+		
+		String headerMsg = getMsg(".active");
+		assertEquals("コース詳細",headerMsg,"パンくずリストの表示名が一致しません");
+			
 	}
 
 }
